49f86aba7d153f72281a2b8db9d5800099c02a09
Abigail <ab841661@ucf.edu>
2026-01-29 19:56:54 -0500

    Update login handler to navigate on login
    Replace console log with navigation to main dashboard.

--- frontend/SafeBitesReactApp/app/Login.tsx
+++ frontend/SafeBitesReactApp/app/Login.tsx
@@ -9,7 +9,7 @@ export default function Login() {
   const [password, setPassword] = useState("");
 
   const handleLogin = () => {
-    console.log({ Username, password });
+    router.push("/main_dashboard");
   };
 
   return (
